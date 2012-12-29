44e507eb02c54b1276dbfe4d577d546213f98b32
Kevin Brandes <kevinbrandes@gmail.com>
2012-12-29 21:49:45 +0100

    + Project setup for dropbox api usage (Download API and ConnectionSettings.plist from Dropbox)

--- SimpleSyncTool/SimpleSyncTool/CloudServices/CloudService.h
+++ SimpleSyncTool/SimpleSyncTool/CloudServices/CloudService.h
@@ -10,19 +10,29 @@
 
 @protocol CloudServiceDelegate <NSObject>
 
+// Delegate gets called when content is ready. Array contains dictionaries with fileinformation
 - (void)didLoadContent:(NSArray *)content ofPath:(NSString *)path;
-- (void)didUploadFileFrom:(NSString *)localPath to:(NSString *)cloudPath;
-- (void)didDownloadFileFrom:(NSString *)cloudPath to:(NSString *)localPath;
+- (void)errorOccurred:(NSError *)error whileLoadingContentOfPath:(NSString *)path;
+// Delegate gets called when a file upload finished
+- (void)didUploadFileFromLocalPath:(NSString *)localPath toCloudPath:(NSString *)cloudPath;
+- (void)errorOccurred:(NSError *)error whileUploadingFileFromLocalPath:(NSString *)localPath toCloudPath:(NSString *)cloudPath;
+// Delegate gets callwhen a file download finished
+- (void)didDownloadFileFromCloudPath:(NSString *)cloudPath toLocalPath:(NSString *)localPath;
+- (void)errorOccurred:(NSError *)error whileDownloadingFileFromCloudPath:(NSString *)cloudPath toLocalPath:(NSString *)localPath;
 
 @end
 
 
 @protocol CloudService <NSObject>
 
-- (void)loadContentOfPath:(NSString *)path;
-- (void)uploadFileFrom:(NSString *)localPath to:(NSString *)cloudPath;
-- (void)downloadFileFrom:(NSString *)cloudPath to:(NSString *)localPath;
+// Load content of a specific path in the cloud (Async)
+- (void)loadContentOfCloudPath:(NSString *)path;
+// Upload a file from the local file system into the cloud (Async)
+- (void)uploadFileFromLocalPath:(NSString *)localPath toCloudPath:(NSString *)cloudPath;
+// Download a file from the cloud into the local file system (Async)
+- (void)downloadFileFromCloudPath:(NSString *)cloudPath toLocalPath:(NSString *)localPath;
 
+// Receive data updates via delegate
 @property (nonatomic, weak) id<CloudServiceDelegate> delegate;
 
 @end
