63bf7b0fcfb509e437165e6cf51072a124c18b99
Kevin Brandes <kevinbrandes@gmail.com>
2013-02-26 21:15:29 +0100

    no message

--- SimpleSyncTool/SimpleSyncTool/CloudServices/Dropbox/DropboxCloudService.m
+++ SimpleSyncTool/SimpleSyncTool/CloudServices/Dropbox/DropboxCloudService.m
@@ -7,11 +7,40 @@
 //
 
 #import "DropboxCloudService.h"
+#import <DropboxOSX/DropboxOSX.h>
 
-@interface DropboxCloudService ()
+#define APPKEY @"DropboxAppKey"
+#define APPSECRET @"DropboxAppSecret"
+
+@interface DropboxCloudService () <DBRestClientOSXDelegate>
+
+@property (nonatomic, strong) DBRestClient *client;
 
 @end
 @implementation DropboxCloudService
+
+# pragma mark - Property getter/setter
+
+- (DBRestClient *)client
+{
+    DBSession *session = [DBSession sharedSession];
+    if (!_client && [session isLinked]) {
+        _client = [[DBRestClient alloc] initWithSession:session];
+        _client.delegate = self;
+    }
+    return _client;
+}
+
+# pragma mark - Public api
+
+// designated initializer
+- (id)initWithAppKey:(NSString *)appKey andAppSecret:(NSString *)appSecret
+{
+    if (self = [super init]) {
+        // initialize object
+    }
+    return self;
+}
 
 - (void)loadContentOfPath:(NSString *)path
 {
@@ -27,5 +56,91 @@
 {
     // Todo: Put logic here
 }
+
+# pragma mark - Private api
+
+- (void)initializeDropboxConnection
+{
+    NSDictionary *connectionSettings = [NSDictionary dictionaryWithContentsOfFile:@"/Users/kevinbrandes/Developer/SimpleSyncTool/SST-DropboxDev/SimpleSyncTool/SimpleSyncTool/ConnectionSettings.plist"];
+    NSString *appKey = [connectionSettings valueForKey:APPKEY];
+    NSString *appSecret = [connectionSettings valueForKey:APPSECRET];
+    DBSession *session = [[DBSession alloc] initWithAppKey:appKey appSecret:appSecret root:kDBRootDropbox];
+    [DBSession setSharedSession:session];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(authHelperStateChangedNotification:) name:DBAuthHelperOSXStateChangedNotification object:[DBAuthHelperOSX sharedHelper]];
+    if (![[DBSession sharedSession] isLinked])
+        [[DBAuthHelperOSX sharedHelper] authenticate];
+}
+
+- (void)authHelperStateChangedNotification:(NSNotification *)notification
+{
+    if ([[DBSession sharedSession] isLinked]) {
+        // You can now start using the API!
+        NSLog(@"Connected");
+    } else {
+        NSLog(@"Disconnected");
+    }
+}
+
+#pragma mark DBRestClientDelegate
+
+- (void)restClient:(DBRestClient*)client loadedMetadata:(DBMetadata*)metadata
+{
+    NSLog(@"Loaded metadata: %@", metadata.contents);
+    for (DBMetadata *content in metadata.contents) {
+        if (content.isDirectory) {
+            NSLog(@"Folder: %@", content.filename);
+        } else {
+            NSLog(@"File: %@", content.filename);
+        }
+    }
+}
+
+- (void)restClient:(DBRestClient*)client metadataUnchangedAtPath:(NSString*)path
+{
+    NSLog(@"Loaded metadata unchanged at path: %@", path);
+}
+
+- (void)restClient:(DBRestClient*)client loadMetadataFailedWithError:(NSError*)error
+{
+    NSLog(@"Loaded metadata failed with error: %@", error);
+}
+
+- (void)restClient:(DBRestClient*)client loadedThumbnail:(NSString*)destPath
+{
+    NSLog(@"Loaded thumbnail: %@", destPath);
+}
+
+- (void)restClient:(DBRestClient*)client loadThumbnailFailedWithError:(NSError*)error
+{
+    NSLog(@"Loaded thumbnail with error: %@", error);
+}
+
+- (void)restClient:(DBRestClient*)client loadedAccountInfo:(DBAccountInfo*)info
+{
+    NSLog(@"Loaded account info: %@", info.displayName);
+}
+
+-(void)restClient:(DBRestClient *)client loadedFile:(NSString *)destPath
+{
+    NSLog(@"loaded File: %@", destPath);
+}
+
+-(void)restClient:(DBRestClient *)client loadFileFailedWithError:(NSError *)error
+{
+    NSLog(@"Load file failed: %@", error);
+}
+
+- (void)restClient:(DBRestClient*)client uploadedFile:(NSString*)destPath
+              from:(NSString*)srcPath metadata:(DBMetadata*)metadata
+{
+    
+    NSLog(@"File uploaded successfully to path: %@", metadata.path);
+}
+
+- (void)restClient:(DBRestClient*)client uploadFileFailedWithError:(NSError*)error
+{
+    NSLog(@"File upload failed with error - %@", error);
+}
+
 
 @end
